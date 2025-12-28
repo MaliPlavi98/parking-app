cdd24322264aea22c6e87d8d1c99e508345bac5a
Ivan Zovkić <izovkic@tvz.hr>
2025-12-28 20:23:39 +0100

    changes

--- src/app/api/settings.js
+++ src/app/api/settings.js
@@ -5,17 +5,21 @@ export function getSettings() {
   return http("GET", "/api/setting");
 }
 
+export function getSettingByKey(key) {
+  return http("GET", `/api/setting/${key}`);
+}
+
 // Create a new setting
-export function createSetting(data, token) {
-  return http("POST", "/api/setting", data, token);
+export function createSetting(data) {
+  return http("POST", "/api/setting", data);
 }
 
 // Update a specific setting by ID
-export function updateSetting(id, data, token) {
-  return http("PUT", `/api/setting/${id}`, data, token);
+export function updateSetting(id, data) {
+  return http("PUT", `/api/setting/${id}`, data);
 }
 
 // Delete a specific setting by ID
-export function deleteSetting(id, token) {
-  return http("DELETE", `/api/setting/${id}`, null, token);
+export function deleteSetting(id) {
+  return http("DELETE", `/api/setting/${id}`);
 }
